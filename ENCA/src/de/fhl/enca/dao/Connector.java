ed3cf8b59b3568f6375467386c0316379695612c
gzwtidus46 <gzwtidus46@outlook.com>
2016-05-28 23:17:33 +0200

    Create class Connector

--- ENCA/src/de/fhl/enca/dao/Connector.java
+++ ENCA/src/de/fhl/enca/dao/Connector.java
@@ -1,5 +1,47 @@
 package de.fhl.enca.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Connector {
 
+	private static Connection connection = null;
+	private static Statement statement = null;
+
+	public static boolean Connect() {
+		try {
+			connection = DriverManager.getConnection("jdbc:sqlite:res\\data\\data.db");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		try {
+			statement = connection.createStatement();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+	public static ResultSet executeSelect(String sql) {
+		try {
+			return statement.executeQuery(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static boolean executeNonSelect(String sql) {
+		try {
+			return statement.execute(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
 }
