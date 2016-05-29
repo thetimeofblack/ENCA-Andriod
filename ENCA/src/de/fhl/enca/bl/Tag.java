8f17f0a0021c62ebf88803e92de5ab267cae7360
gzwtidus46 <gzwtidus46@outlook.com>
2016-05-30 00:40:40 +0200

    Add CABuilder, modify several classes and add columnNumber in DBSpec

--- ENCA/src/de/fhl/enca/bl/Tag.java
+++ ENCA/src/de/fhl/enca/bl/Tag.java
@@ -1,5 +1,97 @@
 package de.fhl.enca.bl;
 
+import java.awt.Color;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Tag {
-	
+
+	public enum tagType_t {
+		room, item, other
+	}
+
+	/* const member */
+	private static final Map<tagType_t, Color> ColorMap = new HashMap<tagType_t, Color>() {
+
+		private static final long serialVersionUID = 5237368492012644125L;
+		{
+			/* Remains to be decided */
+		}
+	};
+
+	private static final Map<tagType_t, String> DescriptionMap = new HashMap<tagType_t, String>() {
+
+		private static final long serialVersionUID = -6613820012277115380L;
+		{
+			/* Remains to be decided */
+		}
+	};
+
+	/* static member */
+	private static Map<Integer, Tag> tagsAll = new HashMap<Integer, Tag>();
+	private static int tagCount;
+
+	/* non-static member */
+	private int tagID;
+	private InternationalString name;
+	private tagType_t tagType;
+
+	private Set<Integer> cleaningAgents = new HashSet<Integer>();
+	private Set<Integer> tagsRelated = new HashSet<Integer>();
+
+	/* static method */
+	public static Tag getTag(int ID) {
+		return tagsAll.get(ID);
+	}
+
+	/* non-static method */
+	public Tag(int tagID, String[] strings, String tagType) {
+		this.tagID = tagID;
+		this.name = new InternationalString(strings);
+		this.tagType = tagType_t.valueOf(tagType);
+		tagsAll.put(tagID, this);
+	}
+
+	public void addCleaningAgent(int id) {
+		cleaningAgents.add(id);
+	}
+
+	public void addRelatedTag(int id) {
+		tagsRelated.add(id);
+	}
+
+	/* getters and setters */
+	public static int getTagCount() {
+		return tagCount;
+	}
+
+	public static void setTagCount(int tagCount) {
+		Tag.tagCount = tagCount;
+	}
+
+	public int getTagID() {
+		return tagID;
+	}
+
+	public void setTagID(int tagID) {
+		this.tagID = tagID;
+	}
+
+	public InternationalString getName() {
+		return name;
+	}
+
+	public void setName(InternationalString name) {
+		this.name = name;
+	}
+
+	public tagType_t getTagType() {
+		return tagType;
+	}
+
+	public void setTagType(tagType_t tagType) {
+		this.tagType = tagType;
+	}
 }
--- ENCA/src/de/fhl/enca/controller/Initialize.java
+++ ENCA/src/de/fhl/enca/controller/Initialize.java
@@ -1,15 +1,75 @@
 package de.fhl.enca.controller;
 
-import java.awt.Toolkit;
 import java.sql.ResultSet;
-import javax.swing.JFrame;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import de.fhl.enca.bl.CleaningAgent;
+import de.fhl.enca.bl.CleaningAgentBuilder;
+import de.fhl.enca.bl.Tag;
 import de.fhl.enca.dao.SQLVisitor;
 
 public final class Initialize {
 
-	private static final Toolkit TOOLKIT = new JFrame().getToolkit();
-
 	public static void initCleaningAgents() {
-		ResultSet resultSet = SQLVisitor.visitCleaningAgentsAll();
+		ResultSet r = SQLVisitor.visitCleaningAgentsAll();
+		try {
+			while (r.next()) {
+				CleaningAgentBuilder builder = new CleaningAgentBuilder();
+				builder.setID(r.getInt(1));
+				builder.setName(new String[] { r.getString(2), r.getString(3), r.getString(4) });
+				builder.setDescription(new String[] { r.getString(5), r.getString(6), r.getString(7) });
+				builder.setinstruction(new String[] { r.getString(8), r.getString(9), r.getString(10) });
+				builder.setApplicationTime(r.getLong(11));
+				builder.setFrequency(r.getLong(12));
+				builder.setType(r.getString(13));
+				builder.setRate(r.getInt(14));
+				builder.setMainLanguage(r.getInt(15));
+				builder.setImage(r.getBytes(16));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void initTags() {
+		ResultSet r = SQLVisitor.visitTagsAll();
+		try {
+			while (r.next()) {
+				new Tag(r.getInt(1), new String[] { r.getString(2), r.getString(3), r.getString(4) }, r.getString(5));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void initRelations() {
+		Map<Integer, Set<Integer>> tcMap = new HashMap<Integer, Set<Integer>>();
+		ResultSet r = SQLVisitor.visitRelations();
+		try {
+			while (r.next()) {
+				int cleaningAgentID = r.getInt(1);
+				int tagID = r.getInt(2);
+				if (!tcMap.containsKey(cleaningAgentID)) {
+					tcMap.put(cleaningAgentID, new HashSet<Integer>());
+				}
+				tcMap.get(cleaningAgentID).add(tagID);
+				CleaningAgent.getCleaningAgent(cleaningAgentID).addTag(tagID);
+				Tag.getTag(tagID).addCleaningAgent(cleaningAgentID);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		for (Set<Integer> group : tcMap.values()) {
+			for (int id1 : group) {
+				for (int id2 : group) {
+					if (id1 != id2 && Tag.getTag(id1).getTagType() != Tag.getTag(id2).getTagType()) {
+						Tag.getTag(id1).addRelatedTag(id2);
+					}
+				}
+			}
+		}
 	}
 }
