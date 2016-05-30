5a16b9aef9a869afa1d11531c6bdd9b5e92857ba
David Zhang <david.zhang311@foxmail.com>
2016-05-30 18:15:41 +0200

    Create TagPreference class

---
+++ ENCA/src/de/fhl/enca/bl/TagPreference.java
@@ -0,0 +1,50 @@
+package de.fhl.enca.bl;
+
+import java.awt.Color;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+/**
+ * @author David
+ * @version 2016-05-30
+ * Class TagPreference
+ * This class contains metadata for tags. It can be stored and read out of
+ * a file.
+ */
+public class TagPreference implements Serializable {
+	private static final long serialVersionUID = -633570705543638913L;
+	
+	private static Map<TagType, Color> colors = new HashMap<TagType, Color>();
+	static {
+		colors.put(TagType.ROOM, Color.getHSBColor(0, 0, 0));
+		colors.put(TagType.ITEM, Color.getHSBColor(0, 0, 0));
+		colors.put(TagType.OTHERS, Color.getHSBColor(0, 0, 0));
+	};
+	private static Map<TagType, InternationalString> descripsions = new HashMap<TagType, InternationalString>();
+	static {
+		// add here
+	};
+	
+	/*
+	 * Getters
+	 */
+	public static Map<TagType, Color> getColors() {
+		return colors;
+	}
+	public static Map<TagType, InternationalString> getDescripsions() {
+		return descripsions;
+	}
+	
+	/*
+	 * Setters
+	 */
+	public static void setColors(Map<TagType, Color> colors) {
+		TagPreference.colors = colors;
+	}
+	public static void setDescripsions(Map<TagType, InternationalString> descripsions) {
+		TagPreference.descripsions = descripsions;
+	}
+}
