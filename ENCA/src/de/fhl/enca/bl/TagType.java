c3a12a85935f802e7d5a6212e07e30bc6ad94c3b
David Zhang <david.zhang311@foxmail.com>
2016-05-30 18:15:54 +0200

    Create TagType Enum

---
+++ ENCA/src/de/fhl/enca/bl/TagType.java
@@ -0,0 +1,28 @@
+package de.fhl.enca.bl;
+
+/**
+ * @author David
+ * @version 2016-05-30
+ * 
+ * Enum TagType
+ * Tags are classified in to three distinctive categories which are: ROOM,
+ * ITEM and OTHERS. TagType enum provides consistent and type-safe marking 
+ * of tag type. It is used globally in this application.
+ */
+public enum TagType {
+	ROOM("Room"), ITEM("Item"), OTHERS("Others");
+	
+	/*
+	 * name: String representation of a type
+	 */
+	String name;
+	
+	private TagType(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return this.name;
+	}
+}
