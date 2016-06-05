f5636bd8380bdfd7326be8b59e753a8146998d29
gzwtidus46 <gzwtidus46@outlook.com>
2016-06-05 23:00:20 +0200

    Format class TagPreference to stop being noticed with a syntax error

--- ENCA/src/de/fhl/enca/bl/TagPreference.java
+++ ENCA/src/de/fhl/enca/bl/TagPreference.java
@@ -5,8 +5,6 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
-
-
 /**
  * @author David
  * @version 2016-05-30
@@ -15,8 +13,9 @@ import java.util.Map;
  * a file.
  */
 public class TagPreference implements Serializable {
+
 	private static final long serialVersionUID = -633570705543638913L;
-	
+
 	private static Map<TagType, Color> colors = new HashMap<TagType, Color>();
 	static {
 		colors.put(TagType.ROOM, Color.getHSBColor(0, 0, 0));
@@ -30,41 +29,43 @@ public class TagPreference implements Serializable {
 		desROOM.setString(LanguageType.CHINESE, "房间");
 		desROOM.setString(LanguageType.ENGLISH, "Room");
 		desROOM.setString(LanguageType.GERMAN, "Zimmer");
-		
+
 		InternationalString desITEM = new InternationalString();
 		desITEM.setString(LanguageType.CHINESE, "物品");
 		desITEM.setString(LanguageType.ENGLISH, "Item");
 		desITEM.setString(LanguageType.GERMAN, "");
 		//remember to add!
-		
+
 		InternationalString desOTHERS = new InternationalString();
 		desOTHERS.setString(LanguageType.CHINESE, "其它");
 		desOTHERS.setString(LanguageType.ENGLISH, "Others");
 		desOTHERS.setString(LanguageType.GERMAN, "");
 		//remember to add!
-		
+
 		descripsions.put(TagType.ROOM, desROOM);
 		descripsions.put(TagType.ITEM, desITEM);
 		descripsions.put(TagType.OTHERS, desOTHERS);
-		
+
 	};
-	
+
 	/*
 	 * Getters
 	 */
 	public static Map<TagType, Color> getColors() {
 		return colors;
 	}
+
 	public static Map<TagType, InternationalString> getDescripsions() {
 		return descripsions;
 	}
-	
+
 	/*
 	 * Setters
 	 */
 	public static void setColors(Map<TagType, Color> colors) {
 		TagPreference.colors = colors;
 	}
+
 	public static void setDescripsions(Map<TagType, InternationalString> descripsions) {
 		TagPreference.descripsions = descripsions;
 	}
