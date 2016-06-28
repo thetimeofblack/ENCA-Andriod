c5ef4bef5e3b5fd9dc0e7c21f4b2c14b03e8b4da
gzwtidus46 <gzwtidus46@outlook.com>
2016-06-28 22:04:37 +0200

    Added Brackets over No Tag

--- ENCA/src/de/fhl/enca/test/ConsoleTest.java
+++ ENCA/src/de/fhl/enca/test/ConsoleTest.java
@@ -1,17 +1,6 @@
 package de.fhl.enca.test;
 
-import de.fhl.enca.bl.LanguageType;
-import de.fhl.enca.bl.Tag;
-import de.fhl.enca.bl.User;
-import de.fhl.enca.controller.Initialize;
-
 public final class ConsoleTest {
 
-	public static void main(String[] args) {
-		User.initialize();
-		Initialize.initialize();
-		for(Tag tag:Tag.getTagsAll()) {
-			System.out.println(tag.getName().getString(LanguageType.GERMAN));
-		}
-	}
+	public static void main(String[] args) {}
 }
