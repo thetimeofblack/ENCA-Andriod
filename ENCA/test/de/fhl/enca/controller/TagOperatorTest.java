1ddf17aa7d79ea7500ff73fe4470bce2c694d2d9
David Zhang <david.zhang311@foxmail.com>
2016-07-16 20:26:58 +0200

    Add tag Operator test

--- ENCA/test/de/fhl/enca/controller/TagOperatorTest.java
+++ ENCA/test/de/fhl/enca/controller/TagOperatorTest.java
@@ -2,7 +2,13 @@ package de.fhl.enca.controller;
 
 import static org.junit.Assert.*;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import de.fhl.enca.bl.InternationalString;
+import de.fhl.enca.bl.LanguageType;
+import de.fhl.enca.bl.Tag;
+import de.fhl.enca.bl.TagType;
 
 /**
  * Class TagOperatorTest tests operations of tag.
@@ -12,9 +18,47 @@ import org.junit.Test;
  */
 public class TagOperatorTest {
 
+	static Tag tag;
+	
+	/**
+	 * Create a test instance of tag
+	 * @throws Exception
+	 */
+	@BeforeClass
+	public static void setUp() throws Exception {
+		InternationalString internationalString;
+		internationalString=new InternationalString();
+		internationalString.setString(LanguageType.CHINESE, "清洁剂Cleaners");
+		internationalString.setString(LanguageType.ENGLISH, "aaacleanersaaa");
+		tag = new Tag(Tag.getMaxID(),internationalString,TagType.getTagType(1),false);
+	}
+		
+	/**
+	 * Test if created
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCreate() {
+		TagOperator.createTag(tag);
+		assertNotNull(Tag.getTag(tag.getTagID()));
+	}
+	
+	/**
+	 * Test modification
+	 */
+	@Test
+	public void testModify() {
+		tag.getName().setString(LanguageType.GERMAN, "Fachhochschule");
+		TagOperator.modifyTag(tag);
+		assertTrue(tag.getName().getString(LanguageType.GERMAN).equals("Fachhochschule"));
+	}
+	
+	/**
+	 * Test and remove the test tag
+	 */
+	@Test
+	public void testRemove() {
+		TagOperator.removeTag(tag);
+		assertFalse(Tag.getTagsAll().contains(tag));
 	}
 
 }
