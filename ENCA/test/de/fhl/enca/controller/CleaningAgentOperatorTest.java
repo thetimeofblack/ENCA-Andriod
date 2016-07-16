6bdcd4e528d36ed61b5db43f77a62d4f0795c6fb
David Zhang <david.zhang311@foxmail.com>
2016-07-16 20:26:33 +0200

    Add Cooperator test

--- ENCA/test/de/fhl/enca/controller/CleaningAgentOperatorTest.java
+++ ENCA/test/de/fhl/enca/controller/CleaningAgentOperatorTest.java
@@ -2,7 +2,12 @@ package de.fhl.enca.controller;
 
 import static org.junit.Assert.*;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import de.fhl.enca.bl.CleaningAgent;
+import de.fhl.enca.bl.InternationalString;
+import de.fhl.enca.bl.LanguageType;
 
 /**
  * Class CleaningAgentOperatorTest tests operations on CAs. 
@@ -11,9 +16,82 @@ import org.junit.Test;
  */
 public class CleaningAgentOperatorTest {
 
+	final static int ID = 20000;
+	static CleaningAgent mCleaningAgent;
+	
+	/**
+	 * Initialize database and create a test instance of CA
+	 */
+	@BeforeClass
+	public static void setup() {
+		Initialize.initialize();
+		
+		mCleaningAgent=new CleaningAgent(ID);
+		
+		InternationalString name=new InternationalString();
+		name.setString(LanguageType.GERMAN, "frosch Glasreiniger");
+		mCleaningAgent.setName(name);
+		
+		InternationalString description=new InternationalString();
+		description.setString(LanguageType.GERMAN, "SODIUM LAURETH SULFATE");
+		mCleaningAgent.setDescription(description);
+		
+		InternationalString instruction=new InternationalString();
+		instruction.setString(LanguageType.GERMAN, "Der Frosch Neutral Reiniger ist ein natürlicher und sehr ergiebiger Allzweckreiniger für die schonende Reinigung von Böden, Holz-, Küchen- und Badmöbeln sowie Kacheln und Sanitäreinrichtungen.");
+		mCleaningAgent.setInstruction(instruction);
+		
+		mCleaningAgent.setApplicationTime(1);
+		
+		mCleaningAgent.setApplicationTime(2);
+		
+		mCleaningAgent.setBelongsToSystem(true);
+		
+		mCleaningAgent.setRate(10);
+		
+		mCleaningAgent.setMainLanguage(LanguageType.GERMAN);
+		
+		mCleaningAgent.setMemo("test Cleaning Agent");
+		
+		CleaningAgentOperator.createCleaningAgent(mCleaningAgent);
+	}
+	
+	/**
+	 * Test if created
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCreate() {
+		assertNotEquals(CleaningAgent.getCleaningAgent(ID), null);
 	}
 
+	/**
+	 * Test if able to save
+	 */
+	@Test
+	public void testSaveMemo() {
+		CleaningAgent ca = CleaningAgent.getCleaningAgent(ID);
+		CleaningAgentOperator.saveMemo(ca, "Test memo");
+		assertEquals(ca.getMemo().equals("Test memo"), true);
+		assertEquals(CleaningAgent.getCleaningAgentsWithMemo().contains(ca), true);
+		CleaningAgentOperator.saveMemo(ca, null);
+		assertEquals(CleaningAgent.getCleaningAgentsWithMemo().contains(ca), false);
+	}
+	
+	/**
+	 * Test if can be modified
+	 */
+	@Test
+	public void testModifyCleaningAgent() {
+		CleaningAgent ca = new CleaningAgent(ID);
+		ca.setMemo("Changed");
+		assertEquals(ca.getMemo().equals("Changed"), true);
+	}
+
+	/**
+	 * Test if successfully removed
+	 */
+	@Test
+	public void testRemove() {
+		CleaningAgentOperator.removeCleaningAgent(CleaningAgent.getCleaningAgent(ID));
+		assertEquals(CleaningAgent.getCleaningAgent(ID), null);
+	}
 }
